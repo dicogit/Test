3414ed0899f2f703045c0bdfe43bf9d3d12f5db4
unknown <abc@xyz.com>
2023-12-11 19:37:54 +0530

    phpdb17

--- compose_phpdb/docker-compose-script.sh
+++ compose_phpdb/docker-compose-script.sh
@@ -2,6 +2,6 @@ sudo yum install docker -y
 sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-#sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 #set -x
 #sudo php=$1 db=$2 docker-compose -f /home/ec2-user/compose_phpdb/docker-compose.yml up -d
